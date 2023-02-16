cdd375df49716dc3a73e4f036912492aa433f502
Ricky Canepa <rickycanepa.rc@gmail.com>
2023-02-16 01:33:47 -0600

    added game manager and game list function

---
+++ src/components/event/EventList.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react"
+import { getEvents } from "../../managers/EventManager"
+
+export const EventList = (props) => {
+    const [ events, setEvents ] = useState([])
+
+    useEffect(() => {
+        getEvents().then(data => setEvents(data))
+    }, [])
+
+    return (
+        <article className="events">
+            {
+                events.map(event => {
+                    return <section key={`game--${game.id}`} className="game">
+                        <div className="game__title">{game.title} by {game.maker}</div>
+                        <div className="game__players">{game.number_of_players} players needed</div>
+                        <div className="game__skillLevel">Skill level is {game.skill_level}</div>
+                    </section>
+                })
+            }
+        </article>
+    )
+}
